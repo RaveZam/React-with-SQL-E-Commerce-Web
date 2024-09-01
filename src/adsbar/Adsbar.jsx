347e6d5fa27a6c71683bc1f764d250233318cb29
Duke <114975455+RaveZam@users.noreply.github.com>
2024-09-01 09:24:08 +0800

    Finished Ads Bar Section

--- src/adsbar/Adsbar.jsx
+++ src/adsbar/Adsbar.jsx
@@ -4,10 +4,33 @@ export default function Adsbar() {
   return (
     <div className={styles.adsbar}>
       <ul className={styles.adsbarul}>
-        <li className={styles.adsbarli}>WorldWide Delivery</li>
-        <li className={styles.adsbarli}>Great Savings</li>
-        <li className={styles.adsbarli}>Gift Vouchers</li>
-        <li className={styles.adsbarli}>24*7 Support</li>
+        <li className={styles.adsbarli}>
+          <img className={styles.icon} src="./images/rocket.png" alt="" />
+          WorldWide Delivery
+          <p className={styles.icondesc}>
+            Ships worldwide with reliable and fast delivery, no matter where you
+            are.
+          </p>
+        </li>
+        <li className={styles.adsbarli}>
+          <img className={styles.icon} src="./images/wallet.png" alt="" /> Great
+          Savings
+          <p className={styles.icondesc}>Save money effortlessly</p>
+        </li>
+        <li className={styles.adsbarli}>
+          <img className={styles.icon} src="./images/box.png" alt="" /> Gift
+          Vouchers
+          <p className={styles.icondesc}>
+            Gift vouchers—ideal for any occasion and perfect for every space
+          </p>
+        </li>
+        <li className={styles.adsbarli}>
+          <img className={styles.icon} src="./images/call.png" alt="" /> 24*7
+          Support
+          <p className={styles.icondesc}>
+            Provide 24/7 Assistance to our valuable customers.
+          </p>
+        </li>
       </ul>
     </div>
   );
